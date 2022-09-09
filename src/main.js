618174dc6b104253e43b43f3c59256d34ed7ba57
Shuvam <shanta.mitra.sm@gmail.com>
2022-09-10 00:25:09 +0530

    implemented carousel on events section

--- src/main.js
+++ src/main.js
@@ -41,3 +41,34 @@ intro.addEventListener('scroll',()=> {
     
 })*/
 
+
+// events page carousel
+
+const slider = document.querySelector('.events-gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
