b5272da460918562bc9e412658e2fd4c0618df99
SoftVibes <chickenpakale@gmail.com>
2022-09-10 14:33:27 +0530

    Added scroll gradient, still need to fix few fonts

--- src/main.js
+++ src/main.js
@@ -82,3 +82,44 @@ var maths_title = new Typed('#club-title', {
     backSpeed : 150,
     showCursor : false
 });
+
+
+//Code for color changing on scroll :-
+function setColor() {
+    theme = document.documentElement.getAttribute('data-theme');
+    
+    if(theme == 'dark') {
+        document.getElementById('container').style.backgroundColor = 'rgb(0, 0, 0)';
+        document.getElementById('container').style.color = 'rgb(255, 255, 255)';
+    } else {
+        document.getElementById('container').style.backgroundColor = 'rgb(255, 255, 255)';
+        document.getElementById('container').style.color = 'rgb(0, 0, 0)';
+    }
+}
+
+setColor();
+
+document.getElementById('container').addEventListener('scroll', function (){
+    theme = document.documentElement.getAttribute('data-theme');
+
+    if (theme == 'dark') {
+        initial = 0;
+        i2 = 255;
+        k = 255;
+        k2 = -255;
+    } else {
+        initial = 255;
+        i2 = 0;
+        k = -255;
+        k2 = 255;
+    }
+
+    height = parseInt(document.getElementById('container').scrollHeight) - parseInt(window.innerHeight);
+    percent = (document.getElementById('container').scrollTop) / height;
+    final = Math.floor(initial + (percent * k));
+    fg = Math.floor(i2 + (percent * k2));
+    console.log(fg);
+
+    document.getElementById('container').style.backgroundColor = 'rgb(' + final + ', ' + final + ', ' + final + ')';
+    document.getElementById('container').style.color = 'rgb(' + fg + ', ' + fg + ', ' + fg + ')';
+});
